cc8ffd06886f85bf980fec78259455c0f59c9099
threepipes <threepipes@gmail.com>
2022-07-06 23:42:26 +0900

    get repo name by git command

--- cmd/ccienv/main.go
+++ cmd/ccienv/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"os"
-	"path/filepath"
+	"os/exec"
+	"regexp"
+	"strings"
 
 	"github.com/alecthomas/kong"
 	"github.com/sirupsen/logrus"
@@ -75,10 +78,30 @@ func handleErr(err error) {
 	}
 }
 
-func getDefaultRepoName() string {
-	path, err := os.Getwd()
-	handleErr(err)
-	return filepath.Base(path)
+func extractRepoName(repo string) (string, string, error) {
+	r := regexp.MustCompile(`.+github\.com[:/]([^/]+)/(.+)\.git/?$`)
+	match := r.FindStringSubmatch(repo)
+	if len(match) < 3 {
+		return "", "", fmt.Errorf("failed to parse repo name: %v", repo)
+	}
+	return match[2], match[1], nil
+}
+
+func getDefaultRepoName() (string, string) {
+	var stderr bytes.Buffer
+	cmd := exec.Command("git", strings.Split("config --get remote.origin.url", " ")...)
+	cmd.Stderr = &stderr
+	out, err := cmd.Output()
+	if err != nil {
+		logrus.Error("Failed to read git remote repository with git command. Please specify the repository by the `-r` option or go to the directory where .git is with git command.")
+		handleErr(err)
+	}
+	repo := strings.TrimSpace(string(out))
+	rn, org, err := extractRepoName(repo)
+	if err != nil {
+		handleErr(err)
+	}
+	return rn, org
 }
 
 func constructProjectSlug(org string, repo string) string {
@@ -91,13 +114,13 @@ func getClient() (*cli.Client, error) {
 		return nil, err
 	}
 
-	repo := cmd.Repo
-	if repo == "" {
-		repo = getDefaultRepoName()
-	}
 	org := cmd.Org
 	if org == "" {
 		org = cfg.OrganizationName
+	}
+	repo := cmd.Repo
+	if repo == "" {
+		repo, org = getDefaultRepoName()
 	}
 
 	slug := constructProjectSlug(org, repo)
@@ -108,7 +131,7 @@ func getClient() (*cli.Client, error) {
 	return client, nil
 }
 
-func main() {
+func mainRun() {
 	kc := kong.Parse(&cmd)
 
 	ctx := context.Background()
@@ -117,4 +140,8 @@ func main() {
 		ClientGenerator: getClient,
 	})
 	handleErr(err)
+}
+
+func main() {
+	mainRun()
 }
---
+++ cmd/ccienv/main_test.go
@@ -0,0 +1,63 @@
+package main
+
+import "testing"
+
+func Test_extractRepoName(t *testing.T) {
+	type args struct {
+		repo string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		want1   string
+		wantErr bool
+	}{
+		{
+			name: "normal case",
+			args: args{
+				repo: "git@github.com:threepipes/circleci-env.git",
+			},
+			want:  "circleci-env",
+			want1: "threepipes",
+		},
+		{
+			name: "trailing slash",
+			args: args{
+				repo: "git@github.com:threepipes/circleci-env.git",
+			},
+			want:  "circleci-env",
+			want1: "threepipes",
+		},
+		{
+			name: "https url",
+			args: args{
+				repo: "https://github.com/user/repo.git",
+			},
+			want:  "repo",
+			want1: "user",
+		},
+		{
+			name: "error pattern",
+			args: args{
+				repo: "repo.git",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := extractRepoName(tt.args.repo)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("extractRepoName() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("extractRepoName() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("extractRepoName() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
